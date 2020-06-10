37e3daac92da0ebe00fc8641538c22c8c041b743
Haeeul <etoilune00@gmail.com>
2020-06-11 07:50:27 +0900

    1st feedback update

--- app/src/main/java/com/example/cadi_kotlin_study/data/follower/FollowerItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cadi_kotlin_study.data.follower
-
-data class FollowerItem(
-    val ImageUrl: String?,
-    val id: String,
-    val name: String
-)
--- app/src/main/java/com/example/cadi_kotlin_study/data/repository/RepoItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cadi_kotlin_study.data.repository
-
-data class RepoItem(
-    val title: String,
-    val desc: String,
-    val language: String?,
-    val color: Int?
-)
